e3ac7a9b690d82fcbc67f3bf647ad024788282d6
Christhonie Geldenhuys <development@christhonie.co.za>
2021-05-14 10:24:39 +0200

    Implement new notify BPMN delegate

---
+++ src/main/java/za/co/dearx/leave/bpmn/delegate/NotifyUser.java
@@ -0,0 +1,47 @@
+package za.co.dearx.leave.bpmn.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.Expression;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import za.co.dearx.leave.bpmn.BPMNUtil;
+import za.co.dearx.leave.service.LeaveApplicationService;
+
+/**
+ * Delegate for a BPMN Service Task to notify a user.
+ * @author Christhonie Geldenhuys
+ *
+ */
+@Component
+public class NotifyUser implements JavaDelegate {
+
+    private final Logger log = LoggerFactory.getLogger(NotifyUser.class);
+
+    @Autowired
+    private LeaveApplicationService leaveApplicationService;
+
+    /**
+     * Optional Input variable from BPMN Service Task.
+     */
+    private Expression status;
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        log.debug("Notify user delegate.");
+
+        if (log.isDebugEnabled()) BPMNUtil.logExecution(log, execution);
+
+        Long applicationId = BPMNUtil.getBusinessKeyAsLong(execution);
+        leaveApplicationService
+            .findOne(applicationId)
+            .ifPresent(
+                application -> {
+                    log.debug("Notifying user " + application.getStaff().getName() + " via email at " + application.getStaff().getEmail());
+                    //TODO Send email
+                }
+            );
+    }
+}
