4c159e44d6124df4f0624f7d885a85a9e0a2433f
Jason van Wyk <jvanwyk@dearx.co.za>
2021-08-25 18:52:43 +0200

    Implement LeaveEntitlement Query
    Implement a query to check if LeaveEntitlement exists for a specific user, leave type and date

--- src/main/java/za/co/dearx/leave/repository/LeaveEntitlementRepository.java
+++ src/main/java/za/co/dearx/leave/repository/LeaveEntitlementRepository.java
@@ -1,11 +1,23 @@
 package za.co.dearx.leave.repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import za.co.dearx.leave.domain.LeaveEntitlement;
+import za.co.dearx.leave.domain.LeaveType;
+import za.co.dearx.leave.domain.Staff;
 
 /**
  * Spring Data SQL repository for the LeaveEntitlement entity.
  */
 @Repository
-public interface LeaveEntitlementRepository extends JpaRepository<LeaveEntitlement, Long>, JpaSpecificationExecutor<LeaveEntitlement> {}
+public interface LeaveEntitlementRepository extends JpaRepository<LeaveEntitlement, Long>, JpaSpecificationExecutor<LeaveEntitlement> {
+    @Query("select le from LeaveEntitlement le where le.entitlementDate = :date and le.staff = :staff and le.leaveType = :leaveType")
+    Optional<LeaveEntitlement> findIfLeaveEntitlementExists(
+        @Param("staff") Staff staff,
+        @Param("date") LocalDate date,
+        @Param("leaveType") LeaveType leaveType
+    );
+}
