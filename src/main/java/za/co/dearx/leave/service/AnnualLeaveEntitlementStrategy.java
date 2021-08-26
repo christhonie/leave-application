d001907889982fadfa82814ebe415c80efb2fe6f
Jason van Wyk <jvanwyk@dearx.co.za>
2021-08-26 09:28:42 +0200

    Implement apply leave entitlement method

--- src/main/java/za/co/dearx/leave/service/AnnualLeaveEntitlementStrategy.java
+++ src/main/java/za/co/dearx/leave/service/AnnualLeaveEntitlementStrategy.java
@@ -1,41 +1,56 @@
 package za.co.dearx.leave.service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.Period;
-
+import za.co.dearx.leave.domain.LeaveEntitlement;
+import za.co.dearx.leave.domain.LeaveType;
 import za.co.dearx.leave.domain.Staff;
 import za.co.dearx.leave.repository.LeaveEntitlementRepository;
 
 public class AnnualLeaveEntitlementStrategy implements ILeaveEntitlementStrategy {
 
     private final LeaveEntitlementRepository leaveEntitlementRepository;
+    private final LeaveType leaveType;
 
     public AnnualLeaveEntitlementStrategy(LeaveEntitlementRepository leaveEntitlementRepository) {
         this.leaveEntitlementRepository = leaveEntitlementRepository;
+        this.leaveType = new LeaveType().id((long) 2);
     }
 
     @Override
     public void apply(Staff staff, LocalDate date) {
-        // TODO Auto-generated method stub
-        //getLeaveEntitleMent for Staff
-        //check if leaveEnt already exists
-        //create leave entitlement
+        leaveEntitlementRepository
+            .findIfLeaveEntitlementExists(staff, date, this.leaveType)
+            .orElseGet(
+                () -> {
+                    return leaveEntitlementRepository.save(
+                        new LeaveEntitlement()
+                            .staff(staff)
+                            .leaveType(this.leaveType)
+                            .entitlementDate(date)
+                            .days(this.getMonthlyLeaveEntitlement(staff))
+                    );
+                }
+            );
     }
 
     @Override
     public byte getLeaveCycleNumber(Staff staff, LocalDate date) {
-    	
         LocalDate start = staff.getStartDate();
-        Period diff = Period.between(
-        		start.withDayOfMonth(1),
-        		date.withDayOfMonth(1));
+        Period diff = Period.between(start.withDayOfMonth(1), date.withDayOfMonth(1));
 
-        int leaveCycleNumber = diff.getMonths()/12 +1;
+        int leaveCycleNumber = diff.getMonths() / 12 + 1;
         return 0; //TODO: Implement Theunis
     }
 
     @Override
     public byte getLeaveCycleMonth(Staff staff, LocalDate date) {
         return 0; //TODO: Implement Theunis
+    }
+
+    public BigDecimal getMonthlyLeaveEntitlement(Staff staff) {
+        return staff.getAnnualLeaveEntitlement().divide(BigDecimal.valueOf(12), 2, RoundingMode.DOWN);
     }
 }
