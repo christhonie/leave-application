414dcd6386ac6dc03f93dab3aecf142522530e7d
Christhonie Geldenhuys <development@christhonie.co.za>
2021-08-27 18:00:30 +0200

    Base changes to display table

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -5,8 +5,10 @@ import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 
+import { TableModule } from 'primeng/table';
+
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), TableModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
